3b94b99045af2e02afcf80942717043e4bcd5faf
Van Kiet <vankiet.1578@gmail.com>
2023-09-21 13:26:21 +0700

    Nop bai tuan 2

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./assets/sass/app.scss";  
+
+import Footer from "./layouts/Footer";
+import Header from './layouts/Header'
+import Main from "./layouts/Main";
+function App() {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+      </>
+  );
+}
+
+export default App;
